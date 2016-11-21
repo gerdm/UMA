1e06cee9442e307f34d6c8491bf17891160ded13
Gerardo Duran <g.duran@me.com>
2016-11-21 06:51:33 -0600

    Edited notebook; finished monkey.py

--- stochastic_processes/final_proyect/infinite_monkey.py
+++ stochastic_processes/final_proyect/infinite_monkey.py
@@ -1,41 +1,52 @@
 import numpy as np
 from numpy.random import choice
 from collections import OrderedDict
+import pickle
 
 alphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".lower().split()
 # The desired stream of words to be written
-target_stream = "wahrscheinlichkeit"
-target_stream = "markov"
+target_stream = "maths"
 
-current_streak = ""
-streak_count = 0
+data_dict = OrderedDict()
 
-streaks_milestone = OrderedDict()
+# Iterate 50 instances of the monkey
+for n_try in range(50):
 
-count = 0
-max_streak = ""
-while True:
-    count += 1
-    # Draw a single uniform sample from the alphabet
-    sample_character = choice(alphabet)
-    if sample_character == target_stream[streak_count]:
-        streak_count += 1
-        current_streak += sample_character
+    current_streak = ""
+    streak_count = 0
+    streaks_milestone = OrderedDict()
+    count = 0
+    max_streak = ""
 
-        # If a new streak is broken, save info
-        if len(current_streak) > len(max_streak):
-            streaks_milestone[current_streak] = count
-            max_streak = current_streak
+    print("---------------------{}------------------------".format(n_try))
+    while True:
+        count += 1
+        # Draw a single uniform sample from the alphabet
+        sample_character = choice(alphabet)
+        if sample_character == target_stream[streak_count]:
+            streak_count += 1
+            current_streak += sample_character
 
-        # Print current state of simulation
-        print(max_streak, count, end="\r")
+            # If a new streak is broken, save streak and iteration number
+            if len(current_streak) > len(max_streak):
+                streaks_milestone[current_streak] = count
+                max_streak = current_streak
 
-    else:
-        streak_count = 0
-        current_streak = ""
+            # Print current state of simulation
+            print(max_streak, count, end="\r")
 
-    if len(current_streak) == len(target_stream):
-        break
+        else:
+            streak_count = 0
+            current_streak = ""
 
+        if len(current_streak) == len(target_stream):
+            break
 
-print(streaks_milestone)
+    print(streaks_milestone)
+
+    # Save current data to data_dict
+    data_dict["round_" + str(n_try)] = streaks_milestone
+
+# Pickle it
+with open("monkey.pickle", "wb") as f:
+    pickle.dump(data_dict, f)
