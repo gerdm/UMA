de8df565cdd15cc1b217249b001382a3c2269599
Gerardo Durán Martín <g.duran@me.com>
2017-04-19 07:25:47 -0500

    Edit spreadsheet; new method to pypremium

--- pensions/pypremium.py
+++ pensions/pypremium.py
@@ -6,21 +6,26 @@ class PensionPremium(object):
     """
     Class to compute the premium for a given family
     with an invalid social worker. The family input is a dictionary
-    of the following form {"invalid": [age, "M", True],
-                           "wife": [age, "F", True],
-                           "son1": [age, "F"/"M", True],
+    of the following form {"name1": ["invalid",age, "M", True],
+                           "name2": ["wife"/ "husband", age, "F", False],
+                           "name3": ["son", age, "F"/"M", False],
                            ...,
-                           "sonn": [age, "F"/"M", True],}
+                           "namen": [family_status, age, sex, Invalid? (T/F)]}
+    In the last example "namen" gives the general form
     """
 
     def __init__(self, family, i_rate):
+        self.database = "./pension_premium.xlsm"
         self.family = family
         self.i_rate = i_rate
+        self.Px_table = pd.read_excel(self.database,
+                                      sheetname="Px", index_col=0)
+        self.PMGs = pd.read_excel(self.database)
 
     def convolution(self, X, Y):
         """
         Let X & Y be two distributions such that Omega(X) = Omega(Y).
-        This function computes the convolution between X & Y
+        This function computes the convolution of X agains Y
         """
         x_dim = len(X)
         Amat = np.zeros([2 * x_dim - 1, x_dim])
@@ -35,8 +40,9 @@ class PensionPremium(object):
 
 
 if __name__ == "__main__":
-    test = PensionPremium(None, None)
-    out = test.convolution(np.array([0.5, 0.1, 0.2, 0.2, 0]),
-                           np.array([0.6, 0.2, 0.1, 0, 0.1]))
-    print(out)
-    
+    family = {"X": ["invalid", 50, "M", True],
+              "Y": ["wife", 45, "F", False],
+              "x1": ["son", 20, "M", False],
+              "x2": ["son", 10, "F", False]}
+
+    test = PensionPremium(family, None)
