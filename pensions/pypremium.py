c90e25f60890a7f4b06c620b4613aee5082e0969
Gerardo Duran <g.duran@me.com>
2017-04-19 21:41:48 -0500

    Added methods to compute the pension premium

--- pensions/pypremium.py
+++ pensions/pypremium.py
@@ -6,21 +6,25 @@ class PensionPremium(object):
     """
     Class to compute the premium for a given family
     with an invalid social worker. The family input is a dictionary
-    of the following form {"name1": ["invalid",age, "M", True],
-                           "name2": ["wife"/ "husband", age, "F", False],
-                           "name3": ["son", age, "F"/"M", False],
+    of the following form {"name1": ["invalid",age, "F"/"M", True],
+                           "name2": ["spouse", age, "F"/"M", False],
+                           "name3": ["descendant", age, "F"/"M", False],
                            ...,
                            "namen": [family_status, age, sex, Invalid? (T/F)]}
     In the last example "namen" gives the general form
     """
-
-    def __init__(self, family, i_rate):
+    def __init__(self, family, CB, i_rate, year):
+        self.CB = CB
+        self.year = year
         self.database = "./pension_premium.xlsm"
         self.family = family
         self.i_rate = i_rate
         self.Px_table = pd.read_excel(self.database,
                                       sheetname="Px", index_col=0)
-        self.PMGs = pd.read_excel(self.database)
+        self.pmgs = pd.read_excel(self.database,
+                                  sheetname="PMG", index_col=0)
+        self.V = 1 / (1 + i_rate)
+
 
     def convolution(self, X, Y):
         """
@@ -39,10 +43,80 @@ class PensionPremium(object):
         return Amat.dot(Y)
 
 
+    def pension_amount(self, number_sons, spouse_alive):
+        """
+        Compute the pension to pay according to the number
+        of sons and if the pensioner has or has not an alive spouse
+        """
+        pmg = self.pmgs.ix[self.year, "PMG"]
+
+        if number_sons == 0 and spouse_alive == False:
+            adjustment_factor = .15
+        else:
+            adjustment_factor = number_sons * 0.10 + spouse_alive * 0.15
+
+        return max(self.CB * (1 + adjustment_factor), pmg)
+
+
+    def map_morttable(self, sex, invalid):
+        """
+        Return the name of the column to map to
+        the mortality table.
+
+        :param sex: "M" or "F"
+        :param invalid: True (if invalid) or False
+        """
+        sex_dict = {"M": "male", "F": "female"}
+
+        if invalid:
+            status = sex_dict[sex] + "_invalid" 
+        else:
+            status = sex_dict[sex] + "_active"
+
+        return status
+
+
+    def prob_x_to_k(self, x, k, sex, invalid):
+        """
+        Compute the probability that a person of age x
+        reaches x + k alive. This is given by the multiplication
+        of k-1 factors: Px * Px+1 *  ... * Px+k-1
+        """
+        col_map = self.map_morttable(sex, invalid)
+        
+        if k == 0:
+            return 1
+        else:
+            probs = self.Px_table.ix[x: x + (k-1), col_map]
+            return np.prod(probs)
+
+
+    def convolute_children(self):
+        """
+        Return the probability distribution of the convolution
+        of livelyhood of children
+        """
+        pass
+
+
+    def annuity_son_pension(self, years, spouse_alive):
+        """
+        Compute the factor of the pension premium that accounts
+        for the probability of live children and whether or not
+        the spouse is alive.
+
+        :param years: number of years of expectancy in livelyhood 
+        :param spouse_alive: whether or not the spouse is alive
+        """
+        pass
+
 if __name__ == "__main__":
     family = {"X": ["invalid", 50, "M", True],
-              "Y": ["wife", 45, "F", False],
-              "x1": ["son", 20, "M", False],
-              "x2": ["son", 10, "F", False]}
+              "Y": ["spouse", 45, "F", False],
+              "x1": ["descendant", 20, "M", False],
+              "x2": ["descendant", 10, "F", False]}
 
-    test = PensionPremium(family, None)
+    test = PensionPremium(family, 3000, 0.35, 2016)
+    print(test.pension_amount(0, True))
+    print(test.pension_amount(1, True))
+    print(test.pension_amount(2, True))
